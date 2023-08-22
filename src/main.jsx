b23a34367af9b8282956772a59ba433257623d6f
Kiranraj27 <kiranraj586@gmail.com>
2023-08-22 08:22:13 +0530

    Web3 context added

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { StateContextProvider } from "./context";
+import { Sepolia } from "@thirdweb-dev/chains";
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <ThirdwebProvider desiredChainId="11155111">
+  <ThirdwebProvider activeChain={Sepolia}>
     <Router>
-      <App />
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
     </Router>
   </ThirdwebProvider>
 );
