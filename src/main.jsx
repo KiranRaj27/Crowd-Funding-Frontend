df1c39b1bd6260c967d684f2d159bcabe6e1d3eb
Kiranraj27 <kiranraj586@gmail.com>
2023-08-22 21:11:41 +0530

    Home page completed

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import { StateContextProvider } from "./context";
-import { Sepolia } from "@thirdweb-dev/chains";
+// import { Sepolia } from "@thirdweb-dev/chains";
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <ThirdwebProvider activeChain={Sepolia}>
+  <ThirdwebProvider desiredChainId={ChainId.Mumbai}> 
     <Router>
       <StateContextProvider>
         <App />
