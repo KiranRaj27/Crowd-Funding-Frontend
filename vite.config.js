df48a0d4dd6a5d663657a5ba7e0546c210c926b2
Kiranraj27 <kiranraj586@gmail.com>
2024-03-08 20:11:13 +0530

    Manifest file changed new

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,37 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugin = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Crowdfunding App",
+    short_name: "Crowd App",
+    description: "Crowdfunding web3 app",
+    icons: [
+      {
+        src: "images/favicon.ico",
+        sizes: "64x64 32x32 24x24 16x16",
+        type: "image/x-icon",
+      },
+      {
+        src: "images/logo192.png",
+        type: "image/png",
+        sizes: "192x192",
+      },
+      {
+        src: "images/logo512.png",
+        type: "image/png",
+        sizes: "512x512",
+      },
+    ],
+    start_url: ".",
+    display: "standalone",
+    theme_color: "#000000",
+    background_color: "#ffffff",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
